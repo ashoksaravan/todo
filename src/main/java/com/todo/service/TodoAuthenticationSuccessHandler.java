f7dcf5f79292f6903090c9f866bece402d3d2096
Ashok Saravanan M <mani.ashoksaravanan@gmail.com>
2012-11-29 15:28:00 +0530

    Added authentication-success-handler

---
+++ src/main/java/com/todo/service/TodoAuthenticationSuccessHandler.java
@@ -0,0 +1,41 @@
+package com.todo.service;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Service;
+
+import com.todo.domain.User;
+
+@Service
+public class TodoAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+	private AuthenticationSuccessHandler target = new SavedRequestAwareAuthenticationSuccessHandler();
+
+	@Autowired
+	UserService service;
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		User user = service.read(authentication.getName());
+		HttpSession session = request.getSession();
+		session.setAttribute("user", user);
+		target.onAuthenticationSuccess(request, response, authentication);
+	}
+
+	public void proceed(HttpServletRequest request, HttpServletResponse response, Authentication auth)
+			throws IOException, ServletException {
+		target.onAuthenticationSuccess(request, response, auth);
+	}
+
+}
